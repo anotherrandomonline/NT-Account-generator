0099c2412e90e440ac55a9a8aeda48ea1ed2bc97
anotherrandomonline <113223065+anotherrandomonline@users.noreply.github.com>
2023-10-05 14:40:25 +0100

    Update config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+NAME = "123456" # The display name of the account
+
+FRIEND_SPAM = False # True = use / False = don't use
+JOIN_TEAM = False # True = use / False = don't use
+
+FRIEND = ["id1", "id2", "id3"] # List of user ids to friend
+TEAMS = ["id1", "id2", "id3"] # List of teams ids to join
